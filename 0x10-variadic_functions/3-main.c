7feb67e76291c22f65f028b149c1e6307c3d25cf
root <sammdeid@gmail.com>
2022-07-07 16:16:33 -0700

    va_args

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code of alx School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  print_all("ceis", 'B', 3, "stSchool");
+  return (0);
+}
